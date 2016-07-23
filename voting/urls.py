22e3d3da15a5d107aff910ea1e653e7b851e4115
Jon Ribbens <jribbens@gmail.com>
2016-07-23 01:57:07 +0100

    Add initial templates and a couple of views

--- voting/urls.py
+++ voting/urls.py
@@ -1,10 +1,22 @@
 """voting URLs"""
 
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 
 from . import views
 
 
+# pylint: disable=invalid-name
+app_name = "voting"
+# pylint: enable=invalid-name
+
+
 urlpatterns = [
-#    url(r"^$", views.home, name="home"),
+    url(r"^$", views.home, name="home"),
+    url(r"^votetakers$", views.votetakers, name="votetakers"),
+
+    # Legacy URLs
+
+    url(r"^who\.php$", RedirectView.as_view(
+        pattern_name="voting:votetakers", permanent=True)),
 ]
--- voting/views.py
+++ voting/views.py
@@ -1,3 +1,22 @@
 """voting views."""
 
 from django.shortcuts import render
+
+from .models import Votetaker
+
+
+def home(request):
+    """Display the home page."""
+    return render(request, "voting/home.html", {})
+
+
+def votetakers(request):
+    """List the votetakers."""
+    return render(
+        request, "voting/votetakers.html", {
+            "active_votetakers":
+                Votetaker.objects.filter(user__is_active=True),
+            "retired_votetakers":
+                Votetaker.objects.filter(user__is_active=False),
+        }
+    )
